e0afbc42cd49286228df268b5be4776a02e84a08
DharaniDJ <nvchinta@asu.edu>
2024-11-26 00:33:18 -0600

    chore: Add CORS configuration and home controller

---
+++ stripe-payment/src/main/java/com/example/stripe_payment/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.stripe_payment.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(@NonNull CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080") // Adjust for your frontend URL
+                .allowedMethods("POST");
+    }
+}
